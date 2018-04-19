87df4b818e7610515805d769691df6f48df0cbf6
Shivam <shivam52548@gmail.com>
2018-04-19 15:38:11 +0530

    Updated Files

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);      // integrating socketio with server
 
 // serve static files
 app.use(express.static(publicPath));
 
+// Socket IO code
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+
+    socket.on('disconnect',()=>{
+      console.log('Client disconnected');
+    });
+});
+
 // listen for incoming requests
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server running at port: ${port}`);
 });
